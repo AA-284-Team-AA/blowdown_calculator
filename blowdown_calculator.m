caaf49876fa13cabb10803c64e913f44be8e1c57
thwhite <thomaswhite4@gmail.com>
2020-02-24 14:11:23 -0800

    Adjusted Cv calculation.

--- blowdown_calculator.m
+++ blowdown_calculator.m
@@ -29,27 +29,30 @@ Mw = 0.0289; % kg/mol Molar weight of gas
 T0 = 298; %K initial 
 P0 = 1000*psi_to_Pa; % Pa
 V_tank = 0.026; % m^3 
-Cd = 1;
-A = 2e-6; % m^2
+% Cd = 1;
+% A = 2e-6; % m^2
+Cv = 0.06
 
 t = linspace(0, 20);
 
 
-
-
-
-%% Calc Flow Velocity Ot of Tank
+%% Calc Flow Velocity Out of Tank
 
 % Choked in regulator at M = 1
 
 c = sqrt(((2*gamma)/(gamma + 1))*(R*T0)/Mw)
 
 
+% Find flow coefficient Cd*A
+
+CdA = Cv * 16.2*mm_to_m^2;
+
+
 %% Create Dischange Coefficient 
 
 c0 = sqrt(gamma*R*T0/Mw)
 
-tau = (V_tank/(Cd*A*c0))*((gamma + 1)/2)^((gamma + 1)/(2*(gamma-1)))
+tau = (V_tank/(CdA*c0))*((gamma + 1)/2)^((gamma + 1)/(2*(gamma-1)))
 
 
 %% Find results
